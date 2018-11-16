fddd5c61e2b3153d12fab53e953bd7980f94e47f
Sami Hattab <hattab_sami@yahoo.fr>
2018-11-16 16:04:47 +0100

    Modification map et combat

--- Map.js
+++ Map.js
@@ -1,11 +1,68 @@
+const diceRollModule = require('./dice.js');
+const AttackModule = require('./AttaqueDefence.js');
+
+let equipment = [];
+
+let inventory = [];
+
+let player = {
+    name : 'John',
+    health : 100,
+    weight : 9,
+    attack : 5,
+    armor : 5 
+}
+let player_strength = 13
+let weight_limit = player_strength*7.5;
 
 
+
+
+function Take(object){
+    if(object.weight + player.weight <= weight_limit){
+        inventory.push(object.name);
+        player.weight += object.weight
+        console.log('Vous avez ramassé ' + object.name);
+    } else {
+        console.log('Cet objet est trop lourd');
+    }
+}
+function Throw(object){
+    if(inventory.indexOf(object.name) != -1){
+        inventory.splice(inventory.indexOf(object.name),1)
+        console.log('Vous avez jeté ' + object.name)
+    } else {
+        console.log('Vous ne pouvez jeter quelquechose que vous n\'avez pas')
+    }
+}
+function Equip(object){
+    if(inventory.indexOf(object.name) != -1){
+        inventory.splice(inventory.indexOf(object.name),1);
+        equipment.push(object.name);
+        player.attack += object.attack;
+        player.armor += object.armor;
+        console.log('Vous êtes maintenant équipé de :' + object.name);
+    } else {
+        console.log('Vous n\'avez pas cet objet');
+    }
+}
+function Unequip(object){
+    if(equipment.indexOf(object.name) != -1){
+        equipment.splice(equipment.indexOf(object.name),1);
+        inventory.push(object.name);
+        player.attack -= object.attack;
+        player.armor -= object.armor;
+        console.log('Vous n\'êtes plus équipé de :' + object.name);
+    } else {
+        console.log('Vous n\'avez rien à déséquiper');
+    }
+}
 //Création de la map du jeu
 let map = [
-    ['_', 'HZ', '_', '_', 'BK'],
-    ['_', '_TRL', '_', '_', '_'],
+    ['_', 'HZ3', '_', 'END', 'BK'],
+    ['_', '_TRL', '_', 'TKV', 'END'],
     ['_', '_', 'CM', '_', '_'],
-    ['HZ', '_', '_', '_TS', '_M'],
+    ['HZ2', '_', '_', '_T', '_M'],
     ['_', '_TR', '_', 'HZ', '_'],
 ];
 
@@ -22,8 +79,26 @@ let mutant = {
     attack : 7,
     armor : 8
 }
+let mutant2 = {
+    name : 'Mutant',
+    health : 50,
+    attack : 7,
+    armor : 8
+}
 
 let zombie = {
+    name : 'Zombie',
+    health : 20,
+    attack : 4,
+    armor : 5
+}
+let zombie2 = {
+    name : 'Zombie',
+    health : 20,
+    attack : 4,
+    armor : 5
+}
+let zombie3 = {
     name : 'Zombie',
     health : 20,
     attack : 4,
@@ -33,13 +108,15 @@ let zombie = {
 let Rifle = {
     name : 'Fusil',
     weight : 6,
-    attack : 13
+    attack : 13,
+    armor : 0
 }
 
 let rocket_launcher = {
     name : 'Lance-Roquette',
     weight : 25,
     attack : 20,
+    armor : 0,
     charges : 1,
     special_effect : function(){
         if(this.charges >= 1){
@@ -51,17 +128,11 @@ let rocket_launcher = {
     }
 }
 
-let seringue = {
-    name : 'Seringue de Santé',
-    weight : 1,
-    charges : 1,
-    special_effect : function(){
-        if(this.charges >= 1){
-            player.health += 100;
-        } else {
-            console.log('Le pouvoir de la potion est épuisé');
-        }
-    }
+let kevlar = {
+    name : 'Protections en Kevlar',
+    weight : 10,
+    attack : 0,
+    armor : 10,
 }
 
 //Position initiale DU Joueur
@@ -156,37 +227,86 @@ function go(direction) {
             console.log("Vous êtes arrivés à la Centrale !" + "[" + joueur.x + "," + joueur.y + "]");
             break;
         case 'BK':
-            console.log(`Vous êtes arrivés au Bunker Nucléaire !
-Vous apercevez le seigneur Karsal` + '[' + joueur.x + ',' + joueur.y + ']');
+            console.log(`Vous êtes arrivés au Bunker Nucléaire !` + '[' + joueur.x + ',' + joueur.y + ']')
+            if(karsal.health > 0){
+                console.log('Vous apercevez le seigneur Karsal !')
+            } else {
+                console.log(`Félicitations, vous avez vaincu le seigneur Karsal et avez par la même occasion récupérez le prototype !
+Vous êtes le sauveur de l'Amérique, vous recevrez par conséquent les honneurs pour vos hauts faits !`)
+            }
             break;
         case '_M':
-            console.log(`Vous êtes sur une plaine désolée !
-Vous apercevez un Mutant !` + '[' + joueur.x + ',' + joueur.y + ']');
+            console.log(`Vous êtes sur une plaine désolée !` + '[' + joueur.x + ',' + joueur.y + ']')
+            if(mutant2.health > 0){
+                console.log(`Vous apercevez un Mutant !`);
+            } else {
+                console.log('Vous retrouvez le cadavre du Mutant');
+            }
             break;
         case 'CM':
-            console.log(`Vous êtes arrivés à la Centrale !
-Vous apercevez un Mutant !` + '[' + joueur.x + ',' + joueur.y + ']');
+            console.log(`Vous êtes arrivés à la Centrale !` + '[' + joueur.x + ',' + joueur.y + ']');
+            if(mutant.health > 0){
+                console.log(`Vous apercevez un Mutant !`);
+            } else {
+                console.log('Vous retrouvez le cadavre du Mutant');
+            }
+            break;
+        case 'END':
+            if(karsal.health <= 0){
+                console.log(`Félicitations, vous avez vaincu le seigneur Karsal et avez par la même occasion récupérez le prototype !
+Vous êtes le sauveur de l'Amérique, vous recevrez par conséquent les honneurs pour vos hauts faits !`)
+            }
+            break;
+        case 'TKV':
+            console.log(`Vous êtes sur une plaine désolée !
+Il y a une épave de camion !` + '[' + joueur.x + ',' + joueur.y + ']');
+            console.log('Vous trouvez une ' + kevlar.name + ' !');
             break;
         case '_TR':
             console.log(`Vous êtes sur une plaine désolée !
-Il y a une épave de camion !` + '[' + joueur.x + ',' + joueur.y + ']');
-            console.log('Vous trouvez un ' + Rifle.name + ' !');
+Il y a une épave de camion !` + '[' + joueur.x + ',' + joueur.y + ']')
+            if(inventory.indexOf(Rifle != -1)){
+                console.log('Vous trouvez un ' + Rifle.name + ' !');
+            }
             break;
         case '_TRL':
             console.log(`Vous êtes sur une plaine désolée !
 Il y a une épave de camion !` + '[' + joueur.x + ',' + joueur.y + ']');
             console.log('Vous trouvez un ' + rocket_launcher.name + ' !');
             break;
-        case '_TS':
+        case '_T':
             console.log(`Vous êtes sur une plaine désolée !
 Il y a une épave de camion !` + '[' + joueur.x + ',' + joueur.y + ']');
-            console.log('Vous trouvez une ' + seringue.name + ' !');
             break;
         case 'HZ':
-            console.log(`Vous êtes dans une maison abandonnée !
-Vous tombez sur un Zombie`  + '[' + joueur.x + ',' + joueur.y + ']');
+            console.log(`Vous êtes dans une maison abandonnée !` + '[' + joueur.x + ',' + joueur.y + ']')
+            if(zombie.health > 0){
+                console.log(`Vous tombez sur un Zombie`);
+            } else {
+                console.log('Vous retrouvez le cadavre du Zombie');
+            }
+            break;
+        case 'HZ2':
+            console.log(`Vous êtes dans une maison abandonnée !` + '[' + joueur.x + ',' + joueur.y + ']')
+            if(zombie2.health > 0){
+                console.log(`Vous tombez sur un Zombie`);
+            } else {
+                console.log('Vous retrouvez le cadavre du Zombie');
+            }
+            break;
+        case 'HZ3':
+            console.log(`Vous êtes dans une maison abandonnée !` + '[' + joueur.x + ',' + joueur.y + ']')
+            if(zombie3.health > 0){
+                console.log(`Vous tombez sur un Zombie`);
+            } else {
+                console.log('Vous retrouvez le cadavre du Zombie');
+            }
             break;
     };
+    function END(){
+        console.log(`Bravo vous avez vaincu le Seigneur Karsal et retrouvez le prototype !
+Vous êtes le sauveur de l'Amérique et recevrez tout les honneurs que vous méritez !`);
+    }
 };
 
 
@@ -224,15 +344,41 @@ let intro =
 Suite à l'explosion d'un prototype de centrale nucléaire ultra développée, l'Amérique a été changée.
 L'explosion de cette centrale a eu l'effet d'une dizaine d'explosion nucléaire partout dans le pays.
 Cependant, ayant prévu ce cas extrème, un groupe composé des meilleurs scientifiques est parvenu à créer
-un prototype de bombe capable de restaurer notre pays à son état d'origine.
+un prototype capable de diffuser une substance dans l'air afin de réprimer les radidations et donc de restaurer notre pays à son état d'origine.
 
-Vous êtes un ancien membre des forces spéciales et avez survécu à l'explosion de la centrale, ayant appris cela
+Vous êtes John, un ancien membre des forces spéciales et avez survécu à l'explosion de la centrale, ayant appris cela
 vous décidez de retrouver ce prototype afin de rapporter à votre pays sa gloire d'antan. Ce prototype serait
 entre les mains d'un puissant mutant s'étant auto-proclamé 'seigneur Karsal'.
 Vous partez donc à la recherche de ce mutant.
 
 
 `;
+
+let rules = `Vous pouvez choisir la direction que vous voulez prendre :
+NORD
+SUD
+EST
+OUEST
+    
+NORD EST
+NORD OUEST
+SUD EST
+SUD OUEST
+
+TAKE + le nom de l'objet pour le ramasser
+EQUIP + le nom de l'objet pour l'équiper
+ATTACK + le nom de l'ennemi pour l'attaquer
+
+Good Luck !
+
+
+`
+
+let suite = 
+`
+Que voulez-vous faire ?
+
+`
 
 
 
@@ -242,36 +388,218 @@ process.stdin.setEncoding('utf8');
 //A chaque entrée dans la console, on appellera la fonction fléchée, rep sera la réponse tapée dans la console
 //Ici on déclare ce que l'on fera lorsqu'on recevra une donée
 process.stdin.on('data', (d) => {	
-	let rep = d.toString().trim() 
-	if(rep == "Ouest"){
-        go('O');
+    let rep = d.toString().trim() 
+    rep = d.toString().trim().toUpperCase();
+	if(rep == "OUEST"){
+        go('O')
 		process.stdin.pause(); //stopper l'entrée
-		
-			slowLog("Voulez vous ramasser le fusil ?\n",100, () => {
-                process.stdin.resume();//réactiver l'entrée à la fin du log
-			});
-	}
-	if(rep == "quit"){
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "EST"){
+        go('E')
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "NORD"){
+        go('N')
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "SUD"){
+        go('S')
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "SUD EST"){
+        go('SE')
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "SUD OUEST"){
+        go('SO')
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "NORD EST"){
+        go('NE')
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "NORD OUEST"){
+        go('NO')
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "ATTACK ZOMBIE" && joueur.x == 3 && joueur.y == 4){
+        AttackModule.Attack(player, zombie);
+        AttackModule.Attack(zombie, player);
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "ATTACK ZOMBIE" && joueur.x == 0 && joueur.y == 3){
+        AttackModule.Attack(player, zombie2);
+        AttackModule.Attack(zombie2, player);
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "ATTACK ZOMBIE" && joueur.x == 1 && joueur.y == 0){
+        AttackModule.Attack(player, zombie3);
+        AttackModule.Attack(zombie3, player);
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "ATTACK MUTANT" && joueur.x == 2 && joueur.y == 2){
+        AttackModule.Attack(player, mutant);
+        AttackModule.Attack(mutant, player);
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "ATTACK MUTANT" && joueur.x == 4 && joueur.y == 3){
+        AttackModule.Attack(player, mutant2);
+        AttackModule.Attack(mutant2, player);
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == "ATTACK KARSAL" && joueur.x == 4 && joueur.y == 0){
+        AttackModule.Attack(player, karsal);
+        AttackModule.Attack(karsal, player);
+		process.stdin.pause(); //stopper l'entrée
+		slowLog(suite,100, () =>{
+			process.stdin.resume();//réactiver l'entrée à la fin du log
+		});
+    }
+    if(rep == 'TAKE FUSIL' && joueur.x == 1 && joueur.y == 4){
+        Take(Rifle);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'TAKE KEVLAR' && joueur.x == 3 && joueur.y == 1){
+        Take(kevlar);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'TAKE LANCE-ROQUETTE' && joueur.x == 1 && joueur.y == 1){
+        Take(rocket_launcher);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'THROW FUSIL' && joueur.x == 1 && joueur.y == 1){
+        Throw(fusil);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'THROW LANCE-ROQUETTE' && joueur.x == 1 && joueur.y == 1){
+        Throw(rocket_launcher);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'THROW KEVLAR' && joueur.x == 1 && joueur.y == 1){
+        Throw(kevlar);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'EQUIP FUSIL'){
+        Equip(Rifle);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'EQUIP LANCE-ROQUETTE'){
+        Equip(rocket_launcher);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'EQUIP KEVLAR'){
+        Equip(kevlar);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'UNEQUIP FUSIL'){
+        Unequip(fusil);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'UNEQUIP LANCE-ROQUETTE'){
+        Unequip(rocket_launcher);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'UNEQUIP KEVLAR   '){
+        Unequip(kevlar);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
+    if(rep == 'STATS'){
+        console.log('Santé : ' + player.health + ' PV');
+        console.log('Dégats : ' + player.attack + ' points de dégats');
+        console.log('Armure : ' + player.armor + ' points d\'armure');
+    }
+	if(rep == "QUIT"){
 		process.exit();
-	}
+    }
+    if(rep == 'HELP'){
+        console.log(rules);
+        process.stdin.pause();
+        slowLog(suite,100, () =>{
+            process.stdin.resume();
+        });
+    }
 });
-
 
 /* début du script ici */
 process.stdin.pause(); //stopper l'entrée pour ne pas pirater le texte
 slowLog(intro,10,()=> {    
-    slowLog(`Dans qu'elle direction voulez-vous allez ?
-Nord
-Sud
-Est
-Ouest
-    
-Nord-Est
-Nord-Ouest
-Sud-Est
-Sud-Ouest
-
-`,10, () => {
+    slowLog(rules,10, () => {
 		process.stdin.resume();//réactiver l'entrée
-	} );
+    });
 });
+
--- inventaire.js
+++ inventaire.js
@@ -1,3 +1,5 @@
+
+
 let equipment = [];
 
 let inventory = [];
